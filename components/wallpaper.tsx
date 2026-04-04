4dfb7960ccb62b976e989eb9270fea5c603a9329
MaenAbabneh <maenababneh@gmail.com>
2026-04-04 20:00:19 +0300

    refactor: remove props from Wallpaper component and utilize zustand for state management

--- components/wallpaper.tsx
+++ components/wallpaper.tsx
@@ -1,4 +1,7 @@
-export default function Wallpaper({ isDarkMode }: { isDarkMode: boolean }) {
+import { useIsDarkMode } from "@/hooks/use-is-dark-mode";
+
+export default function Wallpaper() {
+  const { isDarkMode } = useIsDarkMode();
   return (
     <div
       className="absolute inset-0 bg-cover bg-center transition-all duration-500"
